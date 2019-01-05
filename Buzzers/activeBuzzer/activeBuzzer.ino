19c0c598c42ff22524d34fbc090b7722b3b20f66
P-Gill97 <pgill@csus.edu>
2019-01-04 22:29:31 -0800

    added active buzzer boilerplate code, pitches.h was found on the internet.

---
+++ Buzzers/activeBuzzer/activeBuzzer.ino
@@ -0,0 +1,30 @@
+// will never use a active buzzer but here it is just in case. 
+
+int buzzer = 8; // pin for buzzer
+void setup()
+{
+ pinMode(buzzer,OUTPUT);//initialize buzzer output
+}
+void loop()
+{
+ unsigned char i;
+ while(1)
+ {
+   //output an frequency
+   for(i=0;i<80;i++)
+   {
+    digitalWrite(buzzer,HIGH);
+    delay(1);//wait for 1ms
+    digitalWrite(buzzer,LOW);
+    delay(1);//wait for 1ms
+    }
+    //output another frequency
+     for(i=0;i<100;i++)
+      {
+        digitalWrite(buzzer,HIGH);
+        delay(2);//wait for 2ms
+        digitalWrite(buzzer,LOW);
+        delay(2);//wait for 2ms
+      }
+  }
+}
